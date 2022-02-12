fdf2bd24bb0a57a73955675d9d4a772e5547738c
antoinederoose <antoinederoose@yahoo.fr>
2022-02-12 22:37:56 +0100

    doc and refactoring

--- main.py
+++ main.py
@@ -3,6 +3,14 @@ from math import ceil
 
 
 def drawPawn(posX, posY, color):
+    """
+    Create a pawn on the canvas.
+    This method is used by initPawns().
+    :param posX: the number of the case (0 -> 9) on the X axis
+    :param posY: the number of the case (0 -> 9) on the Y axis
+    :param color: the color of the pawn
+    :return: the ID of the pawn
+    """
     x = (posX * 50) + 5
     y = (posY * 50) + 5
     x1 = (x + 50) - 10
@@ -10,19 +18,14 @@ def drawPawn(posX, posY, color):
     return can.create_oval(x, y, x1, y1, fill=color)
 
 
-def drawBoardLine(lineNumber, offset):
-    for squareNumber in range(0, 5):
-        x = (100 * squareNumber) + offset
-        y = 50 * lineNumber
-        x1 = x + 50
-        y1 = y + 50
-        can.create_rectangle(x, y, x1, y1, fill="#c7784c")
-
-
-def initBoard():
-    # disable the button
-    global initBoardButton, blackLeftCounterLabel, whiteLeftCounterLabel
-    initBoardButton["state"] = DISABLED
+def initPawns():
+    """
+    Init all the pawns for a new game.
+    The ID of the pawns are stored in a list (blackPawns and whitePawns).
+    We also bind to each pawn some callbacks to enable the drag-n-drop mechanism.
+    :return: nothing
+    """
+    global initPawnsButton, blackLeftCounterLabel, whiteLeftCounterLabel
     # place all black
     blackPawns.append(drawPawn(1, 0, "black"))
     blackPawns.append(drawPawn(3, 0, "black"))
@@ -69,41 +72,63 @@ def initBoard():
     for pawnId in blackPawns + whitePawns:
         can.tag_bind(pawnId, '<B1-Motion>', onPawnMoving)
         can.tag_bind(pawnId, '<ButtonRelease-1>', onPawnStopMoving)
-    # set the interface labels
+    # GUI setup
+    initPawnsButton["state"] = DISABLED
     blackLeftCounterLabel["text"] = len(blackPawns)
     whiteLeftCounterLabel["text"] = len(whitePawns)
 
 
-def drawBoard():
+def initBoard():
+    """
+    Create the board on the canvas.
+    :return: nothing
+    """
     for lineNumber in range(0, 10):
         offset = 0 if lineNumber % 2 == 1 else 50
-        drawBoardLine(lineNumber, offset)
+        for squareNumber in range(0, 5):
+            x = (100 * squareNumber) + offset
+            y = 50 * lineNumber
+            x1 = x + 50
+            y1 = y + 50
+            can.create_rectangle(x, y, x1, y1, fill="#c7784c")
 
 
 def onPawnMoving(event):
+    """
+    Callback executed during the pawn movement.
+    Used to recompute its position as the mouse is moving.
+    :param event: used to know the X and Y coordinates of the mouse
+    :return: nothing
+    """
     pawnId = event.widget.find_withtag('current')[0]
     can.coords(pawnId, event.x - 20, event.y - 20, event.x + 20, event.y + 20)
 
 
 def onPawnStopMoving(event):
+    """
+    Callback executed when the mouse is released.
+    Used to correctly place the pawn to the nearest case.
+    :param event: used to know the X and Y coordinates of the mouse
+    :return: nothing
+    """
     pawnId = event.widget.find_withtag('current')[0]
     nearestX = ceil(event.x / 50) * 50
     nearestY = ceil(event.y / 50) * 50
     can.coords(pawnId, nearestX - 45, nearestY - 45, nearestX - 5, nearestY - 5)
 
 
-# our variables
+# variables
 blackPawns = []
 whitePawns = []
 
-# user interface creation
+# GUI
 fen = Tk()
 fen.title("Le jeu de dames")
 fen.resizable(False, False)
 can = Canvas(fen, width=500, height=500, bg="#efcba0")
 can.pack()
-initBoardButton = Button(fen, text="Réinitialiser le jeu", command=initBoard)
-initBoardButton.pack(side=BOTTOM, padx=3, pady=3)
+initPawnsButton = Button(fen, text="Réinitialiser le jeu", command=initPawns)
+initPawnsButton.pack(side=BOTTOM, padx=3, pady=3)
 blackLeftLabel = Label(fen, text="Noirs restants :")
 blackLeftLabel.pack()
 blackLeftCounterLabel = Label(fen, text=0)
@@ -113,12 +138,5 @@ whiteLeftLabel.pack()
 whiteLeftCounterLabel = Label(fen, text=0)
 whiteLeftCounterLabel.pack()
 
-drawBoard()
+initBoard()
 fen.mainloop()
-
-"""
-useful links:
-
-https://stackoverflow.com/questions/54731677/tkinter-canvas-extract-object-id-from-event
-https://stackoverflow.com/questions/32289175/list-of-all-tkinter-events
-"""
